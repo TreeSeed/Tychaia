21ae843bf02f1a7762a911f46f87dee4d9d5a186
Josh Harvey <jharvey@redpointsoftware.com.au>
2012-08-04 23:19:45 +1000

    Added Building Class for town construction.

--- Tychaia.ProceduralGeneration/Biomes/SecondaryBiome.cs
+++ Tychaia.ProceduralGeneration/Biomes/SecondaryBiome.cs
@@ -8,16 +8,24 @@ namespace Tychaia.ProceduralGeneration.Biomes
 {
     public abstract class SecondaryBiome
     {
+        // Requirements for biome placement
         public double MinRainfall;
         public double MaxRainfall;
         public double MinTemperature;
         public double MaxTemperature;
         public double MinTerrain;
         public double MaxTerrain;
-        public double BuildingMaterial;
-        public double TownSuitability = 0;
+
+        // Building assistance
+        public double BuildingMaterial; // The building material that the buildings are made out of in this biome
+        public double HeatValue;        // The amount of heat that is in this biome
+        public double WaterValue;       // The amount of water that is in this biome
+        
+        // Primary to Secondary biomes
         public int[] SuitableBiomes;
         public int DefaultFor = -1;
+
+        // Color that this biome draws
         public Color BrushColor;
     }
 }
