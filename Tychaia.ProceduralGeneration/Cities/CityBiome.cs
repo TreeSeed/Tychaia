a70779bdeb28a12bc231c9219ad8d7bfc72bb5fa
Josh Harvey <jharvey@redpointsoftware.com.au>
2012-11-28 18:26:30 +1100

    City Biomes Changes

--- Tychaia.ProceduralGeneration/Cities/CityBiome.cs
+++ Tychaia.ProceduralGeneration/Cities/CityBiome.cs
@@ -15,6 +15,8 @@ namespace Tychaia.ProceduralGeneration.CityBiomes
         public double MaxOreDensity = 1;
         public double MinRareOreDensity = 0;          // Out of 1.00 (Used for placing prestiege style buildings, such as castles and festival grounds)
         public double MaxRareOreDensity = 1;
+        public double MinAnimalDensity = 0;
+        public double MaxAnimalDensity = 1.0;
         public double MinMilitaryStrength = 0;        // Out of 1.00 (Used for placing military area such as barrcks and training grounds)
         public double MaxMilitaryStrength = 1;
         public double MinWaterValue = 0.0;            // Required water value from the secondary biome for this building to be placed.
