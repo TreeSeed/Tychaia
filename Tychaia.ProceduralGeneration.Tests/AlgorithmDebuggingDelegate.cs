0ccd1d2c6171a055cd16612114b4b31284bb7ed9
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-03-03 11:16:30 +1100

    Added more tests to detect specific zooming bugs.

---
+++ Tychaia.ProceduralGeneration.Tests/AlgorithmDebuggingDelegate.cs
@@ -0,0 +1,45 @@
+//
+// This source code is licensed in accordance with the licensing outlined
+// on the main Tychaia website (www.tychaia.com).  Changes to the
+// license on the website apply retroactively.
+//
+using System;
+using System.Runtime.Serialization;
+using System.ComponentModel;
+
+namespace Tychaia.ProceduralGeneration
+{
+    [DataContract]
+    [FlowDesignerCategory(FlowCategory.Debugging)]
+    [FlowDesignerName("Initial Gradient")]
+    public class AlgorithmDebuggingDelegate : Algorithm<int>
+    {
+        public override bool Is2DOnly
+        {
+            get { return false; }
+        }
+
+        public Func<long, long, long, bool> ValueShouldBePlacedAt
+        {
+            get;
+            set;
+        }
+
+        public override void ProcessCell(IRuntimeContext context, int[] output, long x, long y, long z, int i, int j, int k, int width, int height, int depth, int ox, int oy, int oz)
+        {
+            if (this.ValueShouldBePlacedAt == null)
+                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = 0;
+            else
+                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = this.ValueShouldBePlacedAt(x, y, z) ? 1 : 0;
+        }
+        
+        public override System.Drawing.Color GetColorForValue(StorageLayer parent, dynamic value)
+        {
+            if (value == 0)
+                return System.Drawing.Color.FromArgb(0, 0, 0);
+            else
+                return System.Drawing.Color.FromArgb(255, 0, 0);
+        }
+    }
+}
+
--- Tychaia.ProceduralGeneration.Tests/AlgorithmZoomTest.cs
+++ Tychaia.ProceduralGeneration.Tests/AlgorithmZoomTest.cs
@@ -30,6 +30,110 @@ namespace Tychaia.ProceduralGeneration.Tests
                             i2[i + j * width + k * width * height],
                             "Square zoom is not working for (" + i + ", " + j + ", " + k + ").");
         }
+
+        [Test()]
+        public void TestForCorrectSquareZoomingWithNoOffsetsOrModifications()
+        {
+            int computations;
+            var input = new AlgorithmDebuggingDelegate
+            {
+                ValueShouldBePlacedAt = (x, y, z) => (x == 4 && y == 6 && z == 7)
+            };
+            var zoom = new AlgorithmZoom
+            {
+                Mode = AlgorithmZoom.ZoomType.Square
+            };
+            var runtimeInput = new RuntimeLayer(input);
+            var runtimeZoom = new RuntimeLayer(zoom);
+            runtimeZoom.SetInput(0, runtimeInput);
+            var result = runtimeZoom.GenerateData(0, 0, 0, 32, 32, 32, out computations);
+            
+            // If placed at 4, 6, 7 that will translate to 8, 12, 14 up to 9, 13, 15.
+            Assert.IsTrue(result[8 + 12 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (original point).");
+            Assert.IsTrue(result[9 + 12 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (x, 0, 0 zoomed corner).");
+            Assert.IsTrue(result[8 + 13 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (0, y, 0 zoomed corner).");
+            Assert.IsTrue(result[8 + 12 * 32 + 15 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (0, 0, z zoomed corner).");
+            Assert.IsTrue(result[9 + 13 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (x, y, 0 zoomed corner).");
+            Assert.IsTrue(result[8 + 13 * 32 + 15 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (0, y, z zoomed corner).");
+            Assert.IsTrue(result[9 + 13 * 32 + 15 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (x, y, z zoomed corner).");
+        }
+        
+        [Test()]
+        public void TestForCorrectSquareZoomingWithOffsetX1()
+        {
+            int computations;
+            var input = new AlgorithmDebuggingDelegate
+            {
+                ValueShouldBePlacedAt = (x, y, z) => (x == 4 && y == 6 && z == 7)
+            };
+            var zoom = new AlgorithmZoom
+            {
+                Mode = AlgorithmZoom.ZoomType.Square
+            };
+            var runtimeInput = new RuntimeLayer(input);
+            var runtimeZoom = new RuntimeLayer(zoom);
+            runtimeZoom.SetInput(0, runtimeInput);
+            var result = runtimeZoom.GenerateData(1, 0, 0, 33, 32, 32, out computations);
+            
+            // If placed at 4, 6, 7 that will translate to 7, 12, 14 up to 8, 13, 15.
+            Assert.IsTrue(result[7 + 12 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (original point).");
+            Assert.IsTrue(result[8 + 12 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (x, 0, 0 zoomed corner).");
+            Assert.IsTrue(result[7 + 13 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (0, y, 0 zoomed corner).");
+            Assert.IsTrue(result[7 + 12 * 32 + 15 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (0, 0, z zoomed corner).");
+            Assert.IsTrue(result[8 + 13 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (x, y, 0 zoomed corner).");
+            Assert.IsTrue(result[7 + 13 * 32 + 15 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (0, y, z zoomed corner).");
+            Assert.IsTrue(result[8 + 13 * 32 + 15 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (x, y, z zoomed corner).");
+        }
+        
+        [Test()]
+        public void TestForCorrectSquareZoomingWithOffsetX2()
+        {
+            int computations;
+            var input = new AlgorithmDebuggingDelegate
+            {
+                ValueShouldBePlacedAt = (x, y, z) => (x == 4 && y == 6 && z == 7)
+            };
+            var zoom = new AlgorithmZoom
+            {
+                Mode = AlgorithmZoom.ZoomType.Square
+            };
+            var runtimeInput = new RuntimeLayer(input);
+            var runtimeZoom = new RuntimeLayer(zoom);
+            runtimeZoom.SetInput(0, runtimeInput);
+            var result = runtimeZoom.GenerateData(2, 0, 0, 34, 32, 32, out computations);
+            
+            // If placed at 4, 6, 7 that will translate to 6, 12, 14 up to 7, 13, 15.
+            Assert.IsTrue(result[6 + 12 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (original point).");
+            Assert.IsTrue(result[7 + 12 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (x, 0, 0 zoomed corner).");
+            Assert.IsTrue(result[6 + 13 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (0, y, 0 zoomed corner).");
+            Assert.IsTrue(result[6 + 12 * 32 + 15 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (0, 0, z zoomed corner).");
+            Assert.IsTrue(result[7 + 13 * 32 + 14 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (x, y, 0 zoomed corner).");
+            Assert.IsTrue(result[6 + 13 * 32 + 15 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (0, y, z zoomed corner).");
+            Assert.IsTrue(result[7 + 13 * 32 + 15 * 32 * 32] == 1, "Square zooming is not working with no offsets or modifications (x, y, z zoomed corner).");
+        }
+        
+        [Test()]
+        public void TestForOCXOddAdjustmentShutterBug()
+        {
+            int computations;
+            var input = new AlgorithmDebuggingDelegate
+            {
+                ValueShouldBePlacedAt = (x, y, z) => true
+            };
+            var zoom = new AlgorithmZoom
+            {
+                Mode = AlgorithmZoom.ZoomType.Square
+            };
+            var runtimeInput = new RuntimeLayer(input);
+            var runtimeZoom = new RuntimeLayer(zoom);
+            runtimeZoom.SetInput(0, runtimeInput);
+            var result = runtimeZoom.GenerateData(1, 0, 0, 33, 32, 32, out computations);
+            
+            // We have filled the entire block, therefore this bug can be detected by checking
+            // every odd row.
+            for (var x = 1; x < 32; x += 2)
+                Assert.IsTrue(result[x + 0 * 32 + 0 * 32 * 32] == 1, "OCX odd adjustment shutter bug is present, where every odd row is blank when main adjustment is an odd number.");
+        }
     }
 }
 
