f7d31fa15b85c6c74f8cfb0f14814956a984343d
Josh Harvey <jharvey@redpointsoftware.com.au>
2013-03-15 08:53:11 +1100

    Updated some Algorithms and split some .cs files

--- Tychaia.ProceduralGeneration/AlgorithmMultiply.cs
+++ Tychaia.ProceduralGeneration/AlgorithmMultiply.cs
@@ -3,36 +3,29 @@
 // on the main Tychaia website (www.tychaia.com).  Changes to the
 // license on the website apply retroactively.
 //
+
 using System.Runtime.Serialization;
 using System.ComponentModel;
 using System.Drawing;
 
 namespace Tychaia.ProceduralGeneration
 {
+
     [DataContract]
     [FlowDesignerMajorCategory(FlowMajorCategory.General2D)]
     [FlowDesignerCategory(FlowCategory.Manipulation)]
-    [FlowDesignerName("Value Compare")]
-    public class AlgorithmCompare : Algorithm<int, int, int>
+    [FlowDesignerName("Value Multiply")]
+    public class AlgorithmMultiply : Algorithm<int, int, int>
     {
         [DataMember]
-        [DefaultValue(0)]
-        [Description("Add another 1 per.")]
-        public double AddAnother
-        {
-            get;
-            set;
-        }
-
-        [DataMember]
-        [DefaultValue(20)]
+        [DefaultValue(50)]
         [Description("Estimate maximum value.")]
         public int EstimateMax
         {
             get;
             set;
         }
-
+        
         [DataMember]
         [DefaultValue(false)]
         [Description("This layer is 2d.")]
@@ -41,66 +34,54 @@ namespace Tychaia.ProceduralGeneration
             get;
             set;
         }
-
+        
         public override bool Is2DOnly
         {
             get { return Layer2d; }
         }
-
+        
         public override string[] InputNames
         {
-            get { return new string[] { "Input A", "Input B" }; }
+            get { return new string[] { "Input", "Multiplier" }; }
         }
-
-        public AlgorithmCompare()
-        {
-            this.AddAnother = 0;
-            this.EstimateMax = 20;
+        
+        public AlgorithmMultiply()
+        {   
+            this.EstimateMax = 100;
             this.Layer2d = false;
         }
-
+        
         public override void ProcessCell(IRuntimeContext context, int[] inputA, int[] inputB, int[] output, long x, long y, long z, int i, int j, int k, int width, int height, int depth, int ox, int oy, int oz)
         {
+            double difference = -1;
+
             if (inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height] > inputB[(i + ox) + (j + oy) * width + (k + oz) * width * height])
-            {
-                if (this.AddAnother <= 0)
-                    output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = 1;
-                else
-                    output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = (int)(((double)(inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height] - inputB[(i + ox) + (j + oy) * width + (k + oz) * width * height])) / this.AddAnother);
-            }
+                difference = ((double)(inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height] - inputB[(i + ox) + (j + oy) * width + (k + oz) * width * height]));
+
+            if (difference > 1)
+                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = (int)((double)inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height] * difference);
             else
-                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = 0;
+                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height];
+
         }
         
         public override Color GetColorForValue(StorageLayer parent, dynamic value)
         {
             int a;
 
-            if (this.AddAnother <= 0)
-            {
-                if (value == 1)
-                    a = 255;
-                else
-                    a = 0;
-            }
-            else
-            {
-                double divvalue = (double)this.EstimateMax;
+            double divvalue = (double)this.EstimateMax;
                 
-                if (divvalue > 255)
-                    divvalue = 255;
-                else if (divvalue < 1)
-                    divvalue = 1;
-
-                a = (int)(value * ((double)255 / divvalue));
-
-                if (a > 255)
-                    a = 255;
-            }
-
+            if (divvalue > 255)
+                divvalue = 255;
+            else if (divvalue < 1)
+                divvalue = 1;
+                
+            a = (int)(value * ((double)255 / divvalue));
+                
+            if (a > 255)
+                a = 255;
 
             return Color.FromArgb(a, a, a);
         }
     }
 }
-
---
+++ Tychaia.ProceduralGeneration/AlgorithmZoom2D.cs
@@ -0,0 +1,125 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+using System;
+
+namespace Tychaia.ProceduralGeneration
+{
+    /// <summary>
+    /// "Zooms" in on the 1/4 center region of the parent layer, predicting the most likely
+    /// values for the data that needs to be filled in (since resolution is being doubled).
+    /// Only works in 2 dimensions.
+    /// </summary>
+    [DataContract]
+    [FlowDesignerMajorCategory(FlowMajorCategory.General2D)]
+    [FlowDesignerCategory(FlowCategory.Zooming)]
+    [FlowDesignerName("Zoom 2D")]
+    public class AlgorithmZoom2D : Algorithm<int, int>
+    {
+        [DataMember]
+        [DefaultValue(ZoomType.Square)]
+        [Description("The zooming algorithm to use.")]
+        public ZoomType Mode
+        {
+            get;
+            set;
+        }
+        
+        public override int[] RequiredXBorder { get { return new int[] {2}; } }
+        public override int[] RequiredYBorder { get { return new int[] {2}; } }
+        public override int[] RequiredZBorder { get { return new int[] {0}; } }
+        public override bool[] InputWidthAtHalfSize { get { return new bool[] {true}; } }
+        public override bool[] InputHeightAtHalfSize { get { return new bool[] {true}; } }
+        public override bool[] InputDepthAtHalfSize { get { return new bool[] {false}; } }
+        
+        public AlgorithmZoom2D()
+        {
+            this.Mode = ZoomType.Square;
+        }
+
+        public override string[] InputNames
+        {
+            get { return new string[] { "Input" }; }
+        }
+        
+        public override bool Is2DOnly
+        {
+            get { return true; }
+        }
+        
+        public override void ProcessCell(IRuntimeContext context, int[] input, int[] output, long x, long y, long z, int i, int j, int k, int width, int height, int depth, int ox, int oy, int oz)
+        {
+            int ocx = ((x - i) % 2 == 0 ? 0 : (i % 2));
+            int ocy = ((y - j) % 2 == 0 ? 0 : (j % 2));
+            int ocz = 0;
+            
+            int current = input[(i / 2) + ox + ocx + ((j / 2) + oy + ocy) * width + (k + oz + ocz) * width * height];
+
+            if (this.Mode == ZoomType.Square)
+                output[i + ox + (j + oy) * width + (k + oz) * width * height] = current;
+            else
+            {
+                int selected;
+                
+                bool ymod = (y) % 2 == 0;
+                bool xmod = (x) % 2 == 0;
+
+                if (!xmod && !ymod)
+                if (this.Mode == ZoomType.Fuzzy)
+                    selected = context.GetRandomRange(x, y, 0, 4);
+                else
+                    selected = context.GetRandomRange(x, y, 0, 3);
+                else if (xmod && ymod)
+                    selected = 4;
+                else
+                    selected = context.GetRandomRange(x, y, 0, 2);
+                
+                int ocx_e = ((x - i) % 2 == 0 ? 0 : ((i + 1) % 2));
+                int east = input[((i + 1) / 2 + ox + ocx_e) + (j / 2 + oy + ocy) * width + ((k) + oz + ocz) * width * height];
+
+                switch (selected)
+                {
+                    case 0:
+                        output[i + ox + (j + oy) * width + (k + oz) * width * height] = current;
+                        break;
+                    case 1:
+
+                        int ocy_s = ((y - j) % 2 == 0 ? 0 : ((j + 1) % 2));
+                        int south = input[(i / 2 + ox + ocx) + ((j + 1) / 2 + oy + ocy_s) * width + (k + oz + ocz) * width * height];
+
+                        if (xmod)
+                            output[i + ox + (j + oy) * width + (k + oz) * width * height] = south;
+                        else if (ymod)
+                            output[i + ox + (j + oy) * width + (k + oz) * width * height] = east;
+                        else
+                            output[i + ox + (j + oy) * width + (k + oz) * width * height] = south;
+                        break;
+                    case 2:
+                        output[i + ox + (j + oy) * width + (k + oz) * width * height] = east;
+                        break;
+                    case 3:
+                        int southEast = input[((i + 2) / 2 + ox + ocx) + ((j + 2) / 2 + oy + ocy) * width + (k + oz + ocz) * width * height];
+                        output[i + ox + (j + oy) * width + (k + oz) * width * height] = southEast;
+                        break;
+                    case 4:
+                        output[i + ox + (j + oy) * width + (k + oz) * width * height] = current;
+                        break;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// An enumeration defining the type of zoom to perform.
+        /// </summary>
+        public enum ZoomType
+        {
+            Square,
+            Smooth,
+            Fuzzy,
+        }
+        
+        public override System.Drawing.Color GetColorForValue(StorageLayer parent, dynamic value)
+        {
+            return this.DelegateColorForValueToParent(parent, value);
+        }
+    }
+}
