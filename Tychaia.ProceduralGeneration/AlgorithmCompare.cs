195c5fcda6fac8830437d6b1db9e24181e55b079
Josh Harvey <jharvey@redpointsoftware.com.au>
2013-03-14 19:45:30 +1100

    Added some funky terrain scaling options.

--- Tychaia.ProceduralGeneration/AlgorithmCompare.cs
+++ Tychaia.ProceduralGeneration/AlgorithmCompare.cs
@@ -63,7 +63,7 @@ namespace Tychaia.ProceduralGeneration
         {
             if (inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height] > inputB[(i + ox) + (j + oy) * width + (k + oz) * width * height])
             {
-                if (this.AddAnother == 0)
+                if (this.AddAnother <= 0)
                     output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = 1;
                 else
                     output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = (int)(((double)(inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height] - inputB[(i + ox) + (j + oy) * width + (k + oz) * width * height])) / this.AddAnother);
@@ -76,7 +76,7 @@ namespace Tychaia.ProceduralGeneration
         {
             int a;
 
-            if (this.AddAnother == 0)
+            if (this.AddAnother <= 0)
             {
                 if (value == 1)
                     a = 255;
@@ -99,6 +99,109 @@ namespace Tychaia.ProceduralGeneration
             }
 
 
+            return Color.FromArgb(a, a, a);
+        }
+    }
+
+    [DataContract]
+    [FlowDesignerMajorCategory(FlowMajorCategory.General2D)]
+    [FlowDesignerCategory(FlowCategory.Manipulation)]
+    [FlowDesignerName("Value Compare Multiply")]
+    public class AlgorithmCompareMultiply : Algorithm<int, int, int>
+    {
+        [DataMember]
+        [DefaultValue(0)]
+        [Description("Add another 1 per.")]
+        public double AddAnother
+        {
+            get;
+            set;
+        }
+        
+        [DataMember]
+        [DefaultValue(20)]
+        [Description("Estimate maximum value.")]
+        public int EstimateMax
+        {
+            get;
+            set;
+        }
+        
+        [DataMember]
+        [DefaultValue(false)]
+        [Description("This layer is 2d.")]
+        public bool Layer2d
+        {
+            get;
+            set;
+        }
+        
+        public override bool Is2DOnly
+        {
+            get { return Layer2d; }
+        }
+        
+        public override string[] InputNames
+        {
+            get { return new string[] { "Input A", "Input B" }; }
+        }
+        
+        public AlgorithmCompareMultiply()
+        {
+            this.AddAnother = 0;
+            this.EstimateMax = 100;
+            this.Layer2d = false;
+        }
+        
+        public override void ProcessCell(IRuntimeContext context, int[] inputA, int[] inputB, int[] output, long x, long y, long z, int i, int j, int k, int width, int height, int depth, int ox, int oy, int oz)
+        {
+            double difference = -1;
+
+            if (inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height] > inputB[(i + ox) + (j + oy) * width + (k + oz) * width * height])
+            {
+                if (this.AddAnother <= 0)
+                    difference = ((double)(inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height] - inputB[(i + ox) + (j + oy) * width + (k + oz) * width * height]));
+                else
+                    difference = (((double)(inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height] - inputB[(i + ox) + (j + oy) * width + (k + oz) * width * height])) / this.AddAnother);
+            }
+
+            if (difference > 0)
+            {
+                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = (int)((double)inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height] * difference);
+            }
+            else
+                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = inputA[(i + ox) + (j + oy) * width + (k + oz) * width * height];
+
+
+        }
+        
+        public override Color GetColorForValue(StorageLayer parent, dynamic value)
+        {
+            int a;
+            
+            if (this.AddAnother <= 0)
+            {
+                if (value == 1)
+                    a = 255;
+                else
+                    a = 0;
+            }
+            else
+            {
+                double divvalue = (double)this.EstimateMax;
+                
+                if (divvalue > 255)
+                    divvalue = 255;
+                else if (divvalue < 1)
+                    divvalue = 1;
+                
+                a = (int)(value * ((double)255 / divvalue));
+                
+                if (a > 255)
+                    a = 255;
+            }
+            
+            
             return Color.FromArgb(a, a, a);
         }
     }
