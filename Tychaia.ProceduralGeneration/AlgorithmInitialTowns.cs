cce90efe93436409036c1a05c2c80ba8643a43f8
Josh Harvey <jharvey@redpointsoftware.com.au>
2013-04-20 01:21:17 +1000

    Initial towns.

---
+++ Tychaia.ProceduralGeneration/AlgorithmInitialTowns.cs
@@ -0,0 +1,82 @@
+//
+// This source code is licensed in accordance with the licensing outlined
+// on the main Tychaia website (www.tychaia.com).  Changes to the
+// license on the website apply retroactively.
+//
+using System;
+using System.Runtime.Serialization;
+using System.ComponentModel;
+using System.Drawing;
+using Tychaia.ProceduralGeneration.Towns;
+
+namespace Tychaia.ProceduralGeneration
+{
+    [DataContract]
+    [FlowDesignerMajorCategory(FlowMajorCategory.Specific)]
+    [FlowDesignerCategory(FlowCategory.Towns)]
+    [FlowDesignerName("Initial Towns")]
+    public class AlgorithmInitialTowns : Algorithm<Town>
+    {
+        [DataMember]
+        [DefaultValue(0.9)]
+        [Description("The value between 0.0 and 1.0 above which the cell is selected.")]
+        public double Limit
+        {
+            get;
+            set;
+        }
+        
+        [DataMember]
+        [DefaultValue(true)]
+        [Description("Whether to guarantee a town at the global (0, 0) position.")]
+        public bool GuaranteeStartingPoint
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        [DefaultValue(true)]
+        [Description("Show this layer as 2D in the editor.")]
+        public bool Layer2D
+        {
+            get;
+            set;
+        }
+
+        public override bool Is2DOnly
+        {
+            get { return this.Layer2D; }
+        }
+        
+        public AlgorithmInitialTowns()
+        {
+            this.Limit = 0.9;
+            this.GuaranteeStartingPoint = true;
+            this.Layer2D = true;
+        }
+
+        public override void ProcessCell(IRuntimeContext context, Town[] output, long x, long y, long z, int i, int j, int k, int width, int height, int depth, int ox, int oy, int oz)
+        {
+            if (this.GuaranteeStartingPoint && x == 0 && y == 0)
+                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = new Town();
+            else if (!Layer2D && context.GetRandomDouble(x, y, z, context.Modifier) > this.Limit)
+                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = new Town();
+            else if (Layer2D && context.GetRandomDouble(x, y, 0, context.Modifier) > this.Limit)
+                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = new Town();
+            else
+                output[(i + ox) + (j + oy) * width + (k + oz) * width * height] = null;
+        }
+
+        public override Color GetColorForValue(StorageLayer parent, dynamic value)
+        {
+            if (value == null)
+            {
+                return Color.Gray;
+            }
+
+            return value.BrushColor;
+        }
+    }
+}
+
