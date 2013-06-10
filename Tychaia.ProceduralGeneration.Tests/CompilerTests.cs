e3d2681c00d0d9071535129b04fe9b28631ad570
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-06-10 14:24:05 +1000

    Added more unit tests for the lightspeed compiler.
    Summary:
    This adds more unit tests for the lightspeed compiler, which seems to indicate that after the changes in D40, the existing implementation is working (and resulting in identical output to the runtime layer).
    
    Depends on D40.
    
    Test Plan: Run the unit tests.  It would also be ideal to do some manual verification (maybe hook up `ProceduralGenTracing` to compile the layers) to verify that the output is actually identical and that these unit tests aren't lying.
    
    Reviewers: Sabre070
    
    Reviewed By: Sabre070
    
    Maniphest Tasks: T13
    
    Differential Revision: http://code.redpointsoftware.com.au/D41

--- Tychaia.ProceduralGeneration.Tests/CompilerTests.cs
+++ Tychaia.ProceduralGeneration.Tests/CompilerTests.cs
@@ -34,12 +34,103 @@ namespace Tychaia.ProceduralGeneration.Tests
         }
 
         [Fact]
+        private void ValidatePerlinTest()
+        {
+            var perlin = new RuntimeLayer(new AlgorithmPerlin());
+            ValidateLayer(perlin);
+        }
+
+        [Fact]
         private void ValidatePassthroughTest()
         {
             var initialBool = new RuntimeLayer(new AlgorithmInitialBool());
             var passthrough = new RuntimeLayer(new AlgorithmPassthrough());
             passthrough.SetInput(0, initialBool);
             ValidateLayer(passthrough);
+        }
+
+        [Fact]
+        private void ValidateHeightChangeTest()
+        {
+            var initialBool = new RuntimeLayer(new AlgorithmInitialBool());
+            var heightChange = new RuntimeLayer(new AlgorithmHeightChange());
+            heightChange.SetInput(0, initialBool);
+            ValidateLayer(heightChange);
+        }
+
+        [Fact]
+        private void ValidateZoom2DTest()
+        {
+            var initialBool = new RuntimeLayer(new AlgorithmInitialBool());
+            var zoom2D = new RuntimeLayer(new AlgorithmZoom2D());
+            zoom2D.SetInput(0, initialBool);
+            ValidateLayer(zoom2D);
+        }
+
+        [Fact]
+        private void ValidateZoom2DDoubleTest()
+        {
+            var initialBool = new RuntimeLayer(new AlgorithmInitialBool());
+            var zoom2D1 = new RuntimeLayer(new AlgorithmZoom2D());
+            var zoom2D2 = new RuntimeLayer(new AlgorithmZoom2D());
+            zoom2D1.SetInput(0, initialBool);
+            zoom2D2.SetInput(0, zoom2D1);
+            ValidateLayer(zoom2D2);
+        }
+
+        [Fact]
+        private void ValidateZoom2DTripleTest()
+        {
+            var initialBool = new RuntimeLayer(new AlgorithmInitialBool());
+            var zoom2D1 = new RuntimeLayer(new AlgorithmZoom2D());
+            var zoom2D2 = new RuntimeLayer(new AlgorithmZoom2D());
+            var zoom2D3 = new RuntimeLayer(new AlgorithmZoom2D());
+            zoom2D1.SetInput(0, initialBool);
+            zoom2D2.SetInput(0, zoom2D1);
+            zoom2D3.SetInput(0, zoom2D2);
+            ValidateLayer(zoom2D3);
+        }
+
+        [Fact]
+        private void ValidateZoom3DTest()
+        {
+            var perlin = new RuntimeLayer(new AlgorithmPerlin());
+            var zoom3D = new RuntimeLayer(new AlgorithmZoom3D());
+            zoom3D.SetInput(0, perlin);
+            ValidateLayer(zoom3D);
+        }
+
+        [Fact]
+        private void ValidateZoom3DDoubleTest()
+        {
+            var perlin = new RuntimeLayer(new AlgorithmPerlin());
+            var zoom3D1 = new RuntimeLayer(new AlgorithmZoom3D());
+            var zoom3D2 = new RuntimeLayer(new AlgorithmZoom3D());
+            zoom3D1.SetInput(0, perlin);
+            zoom3D2.SetInput(0, zoom3D1);
+            ValidateLayer(zoom3D2);
+        }
+
+        [Fact]
+        private void ValidateZoom3DTripleTest()
+        {
+            var perlin = new RuntimeLayer(new AlgorithmPerlin());
+            var zoom3D1 = new RuntimeLayer(new AlgorithmZoom3D());
+            var zoom3D2 = new RuntimeLayer(new AlgorithmZoom3D());
+            var zoom3D3 = new RuntimeLayer(new AlgorithmZoom3D());
+            zoom3D1.SetInput(0, perlin);
+            zoom3D2.SetInput(0, zoom3D1);
+            zoom3D3.SetInput(0, zoom3D2);
+            ValidateLayer(zoom3D3);
+        }
+
+        [Fact]
+        private void ValidateExtend2DTest()
+        {
+            var initialBool = new RuntimeLayer(new AlgorithmInitialBool());
+            var extend2D = new RuntimeLayer(new AlgorithmExtend2D());
+            extend2D.SetInput(0, initialBool);
+            ValidateLayer(extend2D);
         }
     }
 }
