c74ee9b6d28cdb7709507c7e14ef1568ff7ea861
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-06-16 22:26:05 +1000

    Added workflow to completely reset and clean the project.
    Summary: This workflow (triggered by `arc force-reset`) causes the entire working directory to be reset and cleaned, and all submodules updated, reset and cleaned back to their expected state.  After running this command, there will be no unstaged or staged changes at all.
    
    Test Plan: Change some files in your working directory and inside some submodules (`Build/cslint` seems to like being modified by MonoDevelop).  After running `arc force-reset` all changes in the working directory should be reset and all build artifacts deleted.  In `Git Extensions`, the "Commit" toolbar icon should display a green tick with 0 pending changes.
    
    Reviewers: Sabre070
    
    Reviewed By: Sabre070
    
    Differential Revision: http://code.redpointsoftware.com.au/D51

---
+++ Build/Arcanist/workflow/RedpointForceResetWorkflow.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Forcibly resets and cleans up the entire project.
+ *
+ * @group workflow
+ */
+final class RedpointForceResetWorkflow extends ArcanistBaseWorkflow {
+
+  public function getWorkflowName() {
+    return 'force-reset';
+  }
+
+  public function getCommandSynopses() {
+    return phutil_console_format(<<<EOTEXT
+      **force-reset**
+EOTEXT
+      );
+  }
+
+  public function getCommandHelp() {
+    return phutil_console_format(<<<EOTEXT
+          Supports: git
+          Completely cleans out the project, all build files and resets all
+          submodules to their correct state as well.
+
+          This will **ERASE** any changes in the local working copy!
+EOTEXT
+      );
+  }
+
+  public function getArguments() {
+    return array(
+    );
+  }
+
+  public function requiresConduit() {
+    return true;
+  }
+
+  public function requiresWorkingCopy() {
+    return true;
+  }
+
+  public function shouldShellComplete() {
+    return false;
+  }
+
+  private function writeOutOkay($console, $msg = null) {
+    if ($msg === null) {
+      $msg = "OKAY";
+    }
+    $console->writeOut("<bg:green> %s </bg>\n", $msg);
+  }
+
+  public function run() {
+    $working_copy = $this->getWorkingCopy();
+    $root = $working_copy->getProjectRoot();
+    $console = PhutilConsole::getConsole();
+
+    // All commands have to execute at the root of the site.
+    chdir($root);
+
+    $console->writeOut("Resetting working directory... ");
+    execx("git reset --hard HEAD");
+    $this->writeOutOkay($console);
+
+    $console->writeOut("Cleaning working directory... ");
+    execx("git clean -xdf");
+    $this->writeOutOkay($console);
+
+    $console->writeOut("Updating submodules... ");
+    execx("git submodule update --init --recursive");
+    $this->writeOutOkay($console);
+
+    $console->writeOut("Resetting submodules... ");
+    execx("git submodule foreach --recursive git reset --hard HEAD");
+    $this->writeOutOkay($console);
+
+    $console->writeOut("Cleaning submodules... ");
+    execx("git submodule foreach --recursive git clean -xdf");
+    $this->writeOutOkay($console);
+
+    $console->writeOut("\n");
+    $this->writeOutOkay($console, "WORKING DIRECTORY RESET");
+
+    return 0;
+  }
+}
