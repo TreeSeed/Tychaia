4a4c1c499d10a273cc1c582944590d0fbec561a1
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-06-30 14:59:19 +1000

    Added reference to syntax CSS file on Redpoint Code.

--- Tychaia.Website/CassetteConfiguration.cs
+++ Tychaia.Website/CassetteConfiguration.cs
@@ -11,23 +11,12 @@ namespace Tychaia.Website
     {
         public void Configure(BundleCollection bundles)
         {
-            // TODO: Configure your bundles here...
-            // Please read http://getcassette.net/documentation/configuration
-
-            // This default configuration treats each file as a separate 'bundle'.
-            // In production the content will be minified, but the files are not combined.
-            // So you probably want to tweak these defaults!
             bundles.Add<StylesheetBundle>("Content");
             bundles.Add<ScriptBundle>("Scripts");
-
-            // To combine files, try something like this instead:
-            //   bundles.Add<StylesheetBundle>("Content");
-            // In production mode, all of ~/Content will be combined into a single bundle.
-
-            // If you want a bundle per folder, try this:
-            //   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
-            // Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
-            // This is useful when there are lots of scripts for different areas of the website.
+            
+            bundles.AddUrlWithAlias<StylesheetBundle>(
+                "http://code.redpointsoftware.com.au/rsrc/css/core/syntax.css",
+                "Scripts");
         }
     }
 }
