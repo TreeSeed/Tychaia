13cee61693cea68754228fd71b304be747741aff
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-08-06 21:37:44 +1000

    Fix for profiler registration.

--- Tychaia/TychaiaIoCModule.cs
+++ Tychaia/TychaiaIoCModule.cs
@@ -6,6 +6,8 @@
 using Ninject.Extensions.Factory;
 using Ninject.Modules;
 using Protogame;
+using Ninject;
+using Ninject.Extensions.Interception.Infrastructure.Language;
 
 namespace Tychaia
 {
@@ -14,28 +16,20 @@ namespace Tychaia
         public override void Load()
         {
             this.Bind<IChunkSizePolicy>().To<DefaultChunkSizePolicy>();
-            //this.Bind<IIsometricifier>().To<DefaultIsometricifier>();
-            //this.Bind<IPerformancePolicy>().To<DefaultPerformancePolicy>();
-            //this.Bind<IRelativeChunkRendering>().To<DefaultRelativeChunkRendering>();
-            //this.Bind<IRenderingBuffers>().To<DefaultRenderingBuffers>().InSingletonScope();
-            //this.Bind<IUniqueRenderCache>().To<DefaultUniqueRenderCache>().InSingletonScope();
             this.Bind<IBackgroundCubeEntityFactory>().ToFactory();
             this.Bind<IChunkOctreeFactory>().ToFactory();
             this.Bind<IChunkFactory>().ToFactory();
             this.Bind<ISkin>().To<TychaiaSkin>();
             this.Bind<IRenderTargetFactory>().To<DefaultRenderTargetFactory>().InSingletonScope();
-            //this.Bind<IIsometricRenderUtilities>().To<DefaultIsometricRenderUtilities>();
-            //this.Bind<IChunkRendererFactory>().ToFactory();
             this.Bind<IChunkProviderFactory>().ToFactory();
-            //this.Bind<ICellRenderOrderCalculator>().To<DefaultCellRenderOrderCalculator>();
             
-#if DEBUG && FALSE
+#if DEBUG
             var profiler = this.Kernel.Get<TychaiaProfiler>();
             this.Bind<IProfiler>().ToMethod(x => profiler);
             this.Bind<TychaiaProfiler>().ToMethod(x => profiler);
             this.Kernel.Intercept(p => p.Request.Service.IsInterface)
                 .With(new TychaiaProfilingInterceptor(profiler));
-#elif RELEASE
+#else
             this.Bind<IProfiler>().To<NullProfiler>().InSingletonScope();
 #endif
         }
