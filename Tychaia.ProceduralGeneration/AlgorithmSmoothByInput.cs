393941ef1726c7f551c7e2cf9703d07b41e83627
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-10-15 19:17:26 +1100

    Remove arrays from AlgorithmSmoothByInput.
    Summary: This removes the array creation in AlgorithmSmoothByInput, addressing T122.  This should reduce the amount of GC hits required when the game is running (although since in-game doesn't use the full generation we can't tell yet).
    
    Test Plan: Make sure the generation still runs properly (test in the world gen viewer to make sure the algorithm is still working).
    
    Reviewers: Sabre070, buildbot
    
    CC: buildbot
    
    Maniphest Tasks: T122
    
    Differential Revision: http://code.redpointsoftware.com.au/D103

--- Tychaia.ProceduralGeneration/AlgorithmSmoothByInput.cs
+++ Tychaia.ProceduralGeneration/AlgorithmSmoothByInput.cs
@@ -81,96 +81,122 @@ namespace Tychaia.ProceduralGeneration
             get { return true; }
         }
 
-        public override void ProcessCell(IRuntimeContext context, int[] inputA, int[] inputB, int[] output, long x,
-            long y, long z, int i, int j, int k, int width, int height, int depth, int ox, int oy, int oz)
+        public override void ProcessCell(
+            IRuntimeContext context,
+            int[] inputA,
+            int[] inputB,
+            int[] output,
+            long x,
+            long y,
+            long z,
+            int i,
+            int j,
+            int k,
+            int width,
+            int height,
+            int depth,
+            int ox,
+            int oy,
+            int oz)
         {
             var ck = (k + oz) * width * height;
             var iox = i + ox;
             var joy = j + oy;
-            var v00 = inputA[(iox - 2) + (joy - 2) * width + ck];
-            var v01 = inputA[(iox - 2) + (joy - 1) * width + ck];
-            var v02 = inputA[(iox - 2) + (joy + 0) * width + ck];
-            var v03 = inputA[(iox - 2) + (joy + 1) * width + ck];
-            var v04 = inputA[(iox - 2) + (joy + 2) * width + ck];
-            var v10 = inputA[(iox - 1) + (joy - 2) * width + ck];
-            var v11 = inputA[(iox - 1) + (joy - 1) * width + ck];
-            var v12 = inputA[(iox - 1) + (joy + 0) * width + ck];
-            var v13 = inputA[(iox - 1) + (joy + 1) * width + ck];
-            var v14 = inputA[(iox - 1) + (joy + 2) * width + ck];
-            var v20 = inputA[(iox + 0) + (joy - 2) * width + ck];
-            var v21 = inputA[(iox + 0) + (joy - 1) * width + ck];
-            var v22 = inputA[(iox + 0) + (joy + 0) * width + ck];
-            var v23 = inputA[(iox + 0) + (joy + 1) * width + ck];
-            var v24 = inputA[(iox + 0) + (joy + 2) * width + ck];
-            var v30 = inputA[(iox + 1) + (joy - 2) * width + ck];
-            var v31 = inputA[(iox + 1) + (joy - 1) * width + ck];
-            var v32 = inputA[(iox + 1) + (joy + 0) * width + ck];
-            var v33 = inputA[(iox + 1) + (joy + 1) * width + ck];
-            var v34 = inputA[(iox + 1) + (joy + 2) * width + ck];
-            var v40 = inputA[(iox + 2) + (joy - 2) * width + ck];
-            var v41 = inputA[(iox + 2) + (joy - 1) * width + ck];
-            var v42 = inputA[(iox + 2) + (joy + 0) * width + ck];
-            var v43 = inputA[(iox + 2) + (joy + 1) * width + ck];
-            var v44 = inputA[(iox + 2) + (joy + 2) * width + ck];
-            var p00 = Math.Max(0, inputB[(iox - 2) + (joy - 2) * width + ck]);
-            var p01 = Math.Max(0, inputB[(iox - 2) + (joy - 1) * width + ck]);
-            var p02 = Math.Max(0, inputB[(iox - 2) + (joy + 0) * width + ck]);
-            var p03 = Math.Max(0, inputB[(iox - 2) + (joy + 1) * width + ck]);
-            var p04 = Math.Max(0, inputB[(iox - 2) + (joy + 2) * width + ck]);
-            var p10 = Math.Max(0, inputB[(iox - 1) + (joy - 2) * width + ck]);
-            var p11 = Math.Max(0, inputB[(iox - 1) + (joy - 1) * width + ck]);
-            var p12 = Math.Max(0, inputB[(iox - 1) + (joy + 0) * width + ck]);
-            var p13 = Math.Max(0, inputB[(iox - 1) + (joy + 1) * width + ck]);
-            var p14 = Math.Max(0, inputB[(iox - 1) + (joy + 2) * width + ck]);
-            var p20 = Math.Max(0, inputB[(iox + 0) + (joy - 2) * width + ck]);
-            var p21 = Math.Max(0, inputB[(iox + 0) + (joy - 1) * width + ck]);
-            var p22 = Math.Max(1, inputB[(iox + 0) + (joy + 0) * width + ck]);
-            var p23 = Math.Max(0, inputB[(iox + 0) + (joy + 1) * width + ck]);
-            var p24 = Math.Max(0, inputB[(iox + 0) + (joy + 2) * width + ck]);
-            var p30 = Math.Max(0, inputB[(iox + 1) + (joy - 2) * width + ck]);
-            var p31 = Math.Max(0, inputB[(iox + 1) + (joy - 1) * width + ck]);
-            var p32 = Math.Max(0, inputB[(iox + 1) + (joy + 0) * width + ck]);
-            var p33 = Math.Max(0, inputB[(iox + 1) + (joy + 1) * width + ck]);
-            var p34 = Math.Max(0, inputB[(iox + 1) + (joy + 2) * width + ck]);
-            var p40 = Math.Max(0, inputB[(iox + 2) + (joy - 2) * width + ck]);
-            var p41 = Math.Max(0, inputB[(iox + 2) + (joy - 1) * width + ck]);
-            var p42 = Math.Max(0, inputB[(iox + 2) + (joy + 0) * width + ck]);
-            var p43 = Math.Max(0, inputB[(iox + 2) + (joy + 1) * width + ck]);
-            var p44 = Math.Max(0, inputB[(iox + 2) + (joy + 2) * width + ck]);
+            var v00 = inputA[(iox - 2) + ((joy - 2) * width) + ck];
+            var v01 = inputA[(iox - 2) + ((joy - 1) * width) + ck];
+            var v02 = inputA[(iox - 2) + ((joy + 0) * width) + ck];
+            var v03 = inputA[(iox - 2) + ((joy + 1) * width) + ck];
+            var v04 = inputA[(iox - 2) + ((joy + 2) * width) + ck];
+            var v10 = inputA[(iox - 1) + ((joy - 2) * width) + ck];
+            var v11 = inputA[(iox - 1) + ((joy - 1) * width) + ck];
+            var v12 = inputA[(iox - 1) + ((joy + 0) * width) + ck];
+            var v13 = inputA[(iox - 1) + ((joy + 1) * width) + ck];
+            var v14 = inputA[(iox - 1) + ((joy + 2) * width) + ck];
+            var v20 = inputA[(iox + 0) + ((joy - 2) * width) + ck];
+            var v21 = inputA[(iox + 0) + ((joy - 1) * width) + ck];
+            var v22 = inputA[(iox + 0) + ((joy + 0) * width) + ck];
+            var v23 = inputA[(iox + 0) + ((joy + 1) * width) + ck];
+            var v24 = inputA[(iox + 0) + ((joy + 2) * width) + ck];
+            var v30 = inputA[(iox + 1) + ((joy - 2) * width) + ck];
+            var v31 = inputA[(iox + 1) + ((joy - 1) * width) + ck];
+            var v32 = inputA[(iox + 1) + ((joy + 0) * width) + ck];
+            var v33 = inputA[(iox + 1) + ((joy + 1) * width) + ck];
+            var v34 = inputA[(iox + 1) + ((joy + 2) * width) + ck];
+            var v40 = inputA[(iox + 2) + ((joy - 2) * width) + ck];
+            var v41 = inputA[(iox + 2) + ((joy - 1) * width) + ck];
+            var v42 = inputA[(iox + 2) + ((joy + 0) * width) + ck];
+            var v43 = inputA[(iox + 2) + ((joy + 1) * width) + ck];
+            var v44 = inputA[(iox + 2) + ((joy + 2) * width) + ck];
+            var p00 = Math.Max(0, inputB[(iox - 2) + ((joy - 2) * width) + ck]);
+            var p01 = Math.Max(0, inputB[(iox - 2) + ((joy - 1) * width) + ck]);
+            var p02 = Math.Max(0, inputB[(iox - 2) + ((joy + 0) * width) + ck]);
+            var p03 = Math.Max(0, inputB[(iox - 2) + ((joy + 1) * width) + ck]);
+            var p04 = Math.Max(0, inputB[(iox - 2) + ((joy + 2) * width) + ck]);
+            var p10 = Math.Max(0, inputB[(iox - 1) + ((joy - 2) * width) + ck]);
+            var p11 = Math.Max(0, inputB[(iox - 1) + ((joy - 1) * width) + ck]);
+            var p12 = Math.Max(0, inputB[(iox - 1) + ((joy + 0) * width) + ck]);
+            var p13 = Math.Max(0, inputB[(iox - 1) + ((joy + 1) * width) + ck]);
+            var p14 = Math.Max(0, inputB[(iox - 1) + ((joy + 2) * width) + ck]);
+            var p20 = Math.Max(0, inputB[(iox + 0) + ((joy - 2) * width) + ck]);
+            var p21 = Math.Max(0, inputB[(iox + 0) + ((joy - 1) * width) + ck]);
+            var p22 = Math.Max(1, inputB[(iox + 0) + ((joy + 0) * width) + ck]);
+            var p23 = Math.Max(0, inputB[(iox + 0) + ((joy + 1) * width) + ck]);
+            var p24 = Math.Max(0, inputB[(iox + 0) + ((joy + 2) * width) + ck]);
+            var p30 = Math.Max(0, inputB[(iox + 1) + ((joy - 2) * width) + ck]);
+            var p31 = Math.Max(0, inputB[(iox + 1) + ((joy - 1) * width) + ck]);
+            var p32 = Math.Max(0, inputB[(iox + 1) + ((joy + 0) * width) + ck]);
+            var p33 = Math.Max(0, inputB[(iox + 1) + ((joy + 1) * width) + ck]);
+            var p34 = Math.Max(0, inputB[(iox + 1) + ((joy + 2) * width) + ck]);
+            var p40 = Math.Max(0, inputB[(iox + 2) + ((joy - 2) * width) + ck]);
+            var p41 = Math.Max(0, inputB[(iox + 2) + ((joy - 1) * width) + ck]);
+            var p42 = Math.Max(0, inputB[(iox + 2) + ((joy + 0) * width) + ck]);
+            var p43 = Math.Max(0, inputB[(iox + 2) + ((joy + 1) * width) + ck]);
+            var p44 = Math.Max(0, inputB[(iox + 2) + ((joy + 2) * width) + ck]);
 
-            var sample = new[,]
-            {
-                { v00, v01, v02, v03, v04 },
-                { v10, v11, v12, v13, v14 },
-                { v20, v21, v22, v23, v24 },
-                { v30, v31, v32, v33, v34 },
-                { v40, v41, v42, v43, v44 }
-            };
-            var applier = new[,]
-            {
-                { p00, p01, p02, p03, p04 },
-                { p10, p11, p12, p13, p14 },
-                { p20, p21, p22, p23, p24 },
-                { p30, p31, p32, p33, p34 },
-                { p40, p41, p42, p43, p44 }
-            };
-
-            var result = 0;
-            var total = 0;
-            var storage = new int[5, 5];
-
-            foreach (var v in applier)
-                total += v;
-            for (var ii = 0; ii < 5; ii++)
-                for (var jj = 0; jj < 5; jj++)
-                    storage[ii, jj] = sample[ii, jj] * applier[ii, jj];
-            foreach (var v in storage)
-                result += v;
-
-            var rounded = (int) (result / (double) total);
+            var total = 
+                p00 + p01 + p02 + p03 + p04 +
+                p10 + p11 + p12 + p13 + p14 +
+                p20 + p21 + p22 + p23 + p24 +
+                p30 + p31 + p32 + p33 + p34 +
+                p40 + p41 + p42 + p43 + p44;
+            
+            var s00 = v00 * p00;
+            var s01 = v01 * p01;
+            var s02 = v02 * p02;
+            var s03 = v03 * p03;
+            var s04 = v04 * p04;
+            var s10 = v10 * p10;
+            var s11 = v11 * p11;
+            var s12 = v12 * p12;
+            var s13 = v13 * p13;
+            var s14 = v14 * p14;
+            var s20 = v20 * p20;
+            var s21 = v21 * p21;
+            var s22 = v22 * p22;
+            var s23 = v23 * p23;
+            var s24 = v24 * p24;
+            var s30 = v30 * p30;
+            var s31 = v31 * p31;
+            var s32 = v32 * p32;
+            var s33 = v33 * p33;
+            var s34 = v34 * p34;
+            var s40 = v40 * p40;
+            var s41 = v41 * p41;
+            var s42 = v42 * p42;
+            var s43 = v43 * p43;
+            var s44 = v44 * p44;
+            
+            var result = 
+                s00 + s01 + s02 + s03 + s04 +
+                s10 + s11 + s12 + s13 + s14 +
+                s20 + s21 + s22 + s23 + s24 +
+                s30 + s31 + s32 + s33 + s34 +
+                s40 + s41 + s42 + s43 + s44;
+            
+            var rounded = (int)(result / (double)total);
             if (rounded < 0)
                 rounded = v22;
-            output[i + ox + (j + oy) * width + (k + oz) * width * height] = rounded;
+            output[i + ox + ((j + oy) * width) + ((k + oz) * width * height)] = rounded;
         }
 
         public override Color GetColorForValue(StorageLayer parent, dynamic value)
